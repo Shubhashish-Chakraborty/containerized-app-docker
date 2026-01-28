2b1aaa18f97181d6bdcffc4917544bd72a7da6e5
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2026-01-28 21:52:19 +0530

    added simple functionalities

--- index.js
+++ index.js
@@ -5,8 +5,17 @@ const PORT = 3001;
 
 app.get("/", (req,res) => {
 	res.send(`
-		<h1>Server is UP!</h1>	
+		<h1>Calculator App is UP! and running!</h1>	
+		<h2> go to /<operation>?a=..&b=.. (add: available) </h2>	
 	`);
+})
+
+app.get("/add", (req, res) => {
+	const firstNumber = parseInt(req.query.a);
+	const secondNumber = parseInt(req.query.b);
+	res.send(`
+		<h1> ${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber} </h1>	
+	`)
 })
 
 app.listen(PORT , () => {
